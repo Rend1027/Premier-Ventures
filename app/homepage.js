640ec58c5fc4d1e94bf479e6e2eca5bdbb14cd1c
rend1027 <rend1027@yahoo.com>
2024-04-04 22:40:03 -0400

    created homepage

---
+++ app/homepage.js
@@ -0,0 +1,9 @@
+
+export default function HomePage() {
+  return (
+    <div>
+      <h1>Premier Stay Ventures</h1>
+      <p>Welcom to your premier destination for luxirous stays. Discover our properties...</p>
+    </div>
+  );
+}
