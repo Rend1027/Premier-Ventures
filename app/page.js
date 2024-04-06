0cb3cbaf4d720746e32e802fd676e63687dfcd4e
rend1027 <rend1027@yahoo.com>
2024-04-06 00:59:23 -0400

    added image to page.js

--- app/page.js
+++ app/page.js
@@ -1,9 +1,19 @@
+import Image from 'next/image';
+
 
 export default function Page() {
   return (
     <div>
-      <h1>Premier Stay Ventures</h1>
-      <p>Welcome to your premier destination for luxirous stays. Discover our properties...</p>
+    <h1>Explore Nearby Attractions</h1>
+    <p>Discover attractions within a 50-mile radius of our property</p>
+    {/* Define inline styles or use a CSS/SCSS file for styling */}
+    
+        <Image
+          src="/images/download.jpg"
+          alt="A beautiful house"
+          width={500}
+          height={500}
+        />
     </div>
   );
 }
