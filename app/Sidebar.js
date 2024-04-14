1ddb236637ef91d7b2e6cafcbb8d802bb362bdad
rend1027 <rend1027@yahoo.com>
2024-04-13 20:32:33 -0400

    added content and css to contact.js

--- app/Sidebar.js
+++ app/Sidebar.js
@@ -1,23 +1,22 @@
-import Link from 'next/link';
-
+import Link from "next/link";
 
 const Sidebar = () => {
-    return (
-        <div className='sidebar min-h-screen fixed left-0 top-0 w-64 bg-gray-800 text-white p-4'>
-            <h2>Navigation</h2>
-            <ul>
-                <li>
-                    <Link href="/">Home</Link>
-                </li>
-                <li>
-                    <Link href="/explore">Explore</Link>
-                </li>
-                <li>
-                    <Link href='/homeowners'>Homeowners</Link>
-                </li>
-            </ul>
-        </div>
-    );
+  return (
+    <div className="sidebar min-h-screen fixed left-0 top-0 w-64 bg-gray-800 text-white p-4">
+      <h2>Navigation</h2>
+      <ul>
+        <li>
+          <Link href="/">Home</Link>
+        </li>
+        <li>
+          <Link href="/contact">Contact</Link>
+        </li>
+        <li>
+          <Link href="/homeowners">Homeowners</Link>
+        </li>
+      </ul>
+    </div>
+  );
 };
 
 export default Sidebar;
--- app/contact/page.js
+++ app/contact/page.js
@@ -1,10 +1,79 @@
 const Contact = () => {
-    return (
-        <div>
-            <h1>Explore Nearby Atrractions</h1>
-            <p>Discover attraction withing a 50-mile radius of our property</p>
-        </div>
-    );
+  return (
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "100vh",
+        padding: "20px",
+      }}
+    >
+      <p style={{ fontSize: "20px", marginBottom: "15px" }}>
+        {" "}
+        Please contact us with any questions or inquiries you might have and we
+        will get back to you as soon as possible!
+      </p>
+      <form
+        style={{
+          backgroundColor: "silver",
+          width: "50%",
+          height: "500px",
+          borderRadius: "10px",
+          alignContent: "center",
+          paddingLeft: "180px",
+        }}
+      >
+        <section>
+          <label htmlFor="name">Name:</label>
+          <input
+            type="text"
+            id="name"
+            name="name"
+            placeholder="First Name"
+          ></input>
+          <input style={{ marginLeft: "10px" }} placeholder="Last Name"></input>
+        </section>
+        <br />
+        <section>
+          <label htmlFor="email">Email:</label>
+          <input
+            type="email"
+            id="email"
+            name="email"
+            placeholder="Enter Email"
+          ></input>
+        </section>
+        <br />
+        <section>
+          <label htmlFor="subject">Subject:</label>
+          <input
+            type="text"
+            id="subject"
+            name="subject"
+            placeholder="Enter Subject"
+          ></input>
+        </section>
+        <br />
+        <section>
+          <label htmlFor="message">Message:</label>
+          <br />
+          <textarea rows="4" cols="50" />
+        </section>
+        <br />
+        <button
+          style={{
+            backgroundColor: "gray",
+            borderRadius: "5px",
+            padding: "10px 20px",
+          }}
+        >
+          Submit
+        </button>
+      </form>
+    </div>
+  );
 };
 
-export default ExplorePage;
+export default Contact;
