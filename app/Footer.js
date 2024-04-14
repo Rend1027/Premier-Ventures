ae33e2e6fea6cabaa5b29a82971d4ea319c87016
rend1027 <rend1027@yahoo.com>
2024-04-14 00:44:35 -0400

    edit

--- app/Footer.js
+++ app/Footer.js
@@ -2,6 +2,8 @@ const Footer = () => {
   return (
     <footer
       style={{
+        display: "flex",
+        flexDirection: "column",
         padding: "20px",
         backgroundColor: "#f0f0f0",
         textAlign: "center",
@@ -9,10 +11,13 @@ const Footer = () => {
       }}
     >
       <section>
-        <p>Be In Touch</p>
         <address>
           Premier Stay Ventures LLC. 2085 Lexington Ave. New York, NY. 10029
         </address>
+        <tele>917-860-9106</tele>
+        <br/>
+        <email>psventures@gmail.com</email>
+        <p>©2024</p>
       </section>
     </footer>
   );
--- app/Sidebar.js
+++ app/Sidebar.js
@@ -3,7 +3,6 @@ import Link from "next/link";
 const Sidebar = () => {
   return (
     <div className="sidebar min-h-screen fixed left-0 top-0 w-64 bg-gray-800 text-white p-4">
-      <h2>Navigation</h2>
       <ul>
         <li>
           <Link href="/">Home</Link>
